5f2a458218914ef14169d92cbc0bfca8d02b4a7e
pfoof <piotr@pabis.eu>
2020-08-27 17:49:59 +0200

    `Endpoint::retrofit` is not nullable!

--- app/src/main/java/dog/snow/androidrecruittest/repository/Endpoint.kt
+++ app/src/main/java/dog/snow/androidrecruittest/repository/Endpoint.kt
@@ -5,8 +5,8 @@ import dog.snow.androidrecruittest.R
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class Endpoint(val context: Context) {
-    val retrofit = Retrofit.Builder()
+class Endpoint(context: Context) {
+    val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(context.resources.getString(R.string.base_url))
         .addConverterFactory(GsonConverterFactory.create())
         .build()
