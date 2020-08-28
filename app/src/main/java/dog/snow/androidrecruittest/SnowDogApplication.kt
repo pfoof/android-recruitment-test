ee1b995d4abc4d3b0379a552d669e113fd3cb61e
pfoof <piotr@pabis.eu>
2020-08-28 17:16:29 +0200

    Added more injections for comfortable usage

--- app/src/main/java/dog/snow/androidrecruittest/SnowDogApplication.kt
+++ app/src/main/java/dog/snow/androidrecruittest/SnowDogApplication.kt
@@ -2,6 +2,10 @@ package dog.snow.androidrecruittest
 
 import android.app.Application
 import dog.snow.androidrecruittest.repository.Endpoint
+import dog.snow.androidrecruittest.repository.service.AlbumService
+import dog.snow.androidrecruittest.repository.service.PhotoService
+import dog.snow.androidrecruittest.repository.service.UserService
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.android.startKoin
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module.module
@@ -9,7 +13,10 @@ import org.koin.dsl.module.module
 class SnowDogApplication: Application() {
 
     private val endpointModule = module {
-        single<Endpoint> { Endpoint(androidContext()) }
+        single { Endpoint(androidContext()) }
+        single<PhotoService> { inject<Endpoint>().value.retrofit.create(PhotoService::class.java) }
+        single<AlbumService> { inject<Endpoint>().value.retrofit.create(AlbumService::class.java) }
+        single<UserService> { inject<Endpoint>().value.retrofit.create(UserService::class.java) }
     }
 
     override fun onCreate() {
