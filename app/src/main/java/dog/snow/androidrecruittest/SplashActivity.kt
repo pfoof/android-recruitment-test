fa6517f3f3a68b2dc56d79ca2ffc1f6ab785725b
pfoof <piotr@pabis.eu>
2020-08-28 17:17:11 +0200

    Check network activity and show error. Also launch fetching in coroutines

--- app/src/main/java/dog/snow/androidrecruittest/SplashActivity.kt
+++ app/src/main/java/dog/snow/androidrecruittest/SplashActivity.kt
@@ -4,21 +4,41 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import dog.snow.androidrecruittest.extensions.hasNetwork
 import dog.snow.androidrecruittest.repository.Endpoint
+import dog.snow.androidrecruittest.repository.model.RawPhoto
+import dog.snow.androidrecruittest.repository.service.PhotoService
+import kotlinx.coroutines.*
 import org.koin.android.ext.android.inject
 
 class SplashActivity : AppCompatActivity(R.layout.splash_activity) {
 
-    private val endpoint:Endpoint by inject()
+    private val photoService: PhotoService by inject()
+    private val parentJob = Job()
+    private val coroutineScope = CoroutineScope(parentJob + Dispatchers.IO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if(endpoint.retrofit != null)
-            Toast.makeText(this, "Endpoint created", Toast.LENGTH_SHORT).show()
-        else
-            Toast.makeText(this, "Endpoint NOT created!!!", Toast.LENGTH_SHORT).show()
+
     }
+
+    private fun tryDownload() {
+        if(!hasNetwork) {
+            showError("All networks are down")
+            return
+        }
+
+        coroutineScope.launch {
+            val photos = getPhotosAsync().await()
+
+        }
+    }
+
+    private fun getPhotosAsync(): Deferred<List<RawPhoto>?> =
+        coroutineScope.async(Dispatchers.IO) {
+            return@async photoService.getPhotos(100).execute().body()
+        }
 
     private fun showError(errorMessage: String?) {
         MaterialAlertDialogBuilder(this)
