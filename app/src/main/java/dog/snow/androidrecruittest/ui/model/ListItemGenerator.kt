fad5e4413dcab2f0308207478b8535c5de77e844
pfoof <piotr@pabis.eu>
2020-08-29 13:51:21 +0200

    Create `ListItem`s from photos and albums collections

---
+++ app/src/main/java/dog/snow/androidrecruittest/ui/model/ListItemGenerator.kt
@@ -0,0 +1,10 @@
+package dog.snow.androidrecruittest.ui.model
+
+import dog.snow.androidrecruittest.repository.model.RawAlbum
+import dog.snow.androidrecruittest.repository.model.RawPhoto
+import java.util.*
+
+object ListItemGenerator {
+    fun generateFromPhotosAndAlbums(photos:List<RawPhoto>, albums: Map<Int, RawAlbum>):List<ListItem> =
+        photos.map { p -> ListItem(p.id, p.title, albums[p.albumId]!!.title, p.thumbnailUrl) }
+}
