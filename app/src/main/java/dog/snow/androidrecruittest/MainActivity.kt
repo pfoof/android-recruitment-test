6e41332dd1dadfbf0c14090336db73d374988d81
pfoof <piotr@pabis.eu>
2020-08-29 22:22:21 +0200

    Added view model observers to the `ListFragment`. Binded adapter with `RecyclerView`. TODO: why is the `RecyclerView` not updating after notifying? The list submitted is changing definitely in the adapter but the `RecyclerView` doesn't seem to care.

--- app/src/main/java/dog/snow/androidrecruittest/MainActivity.kt
+++ app/src/main/java/dog/snow/androidrecruittest/MainActivity.kt
@@ -7,20 +7,27 @@ import dog.snow.androidrecruittest.repository.model.RawAlbum
 import dog.snow.androidrecruittest.repository.model.RawPhoto
 import dog.snow.androidrecruittest.repository.model.RawUser
 import dog.snow.androidrecruittest.ui.model.ListItem
+import dog.snow.androidrecruittest.viewmodels.AlbumsViewModel
 import dog.snow.androidrecruittest.viewmodels.ListItemsViewModel
+import dog.snow.androidrecruittest.viewmodels.PhotosViewModel
+import dog.snow.androidrecruittest.viewmodels.UsersViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.util.*
 
 class MainActivity : AppCompatActivity(R.layout.main_activity){
 
-    private val dataViewModel: ListItemsViewModel by viewModel()
+    private val albumsViewModel: AlbumsViewModel by viewModel()
+    private val photosViewModel: PhotosViewModel by viewModel()
+    private val usersViewModel: UsersViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-
+        // Most optimistic order
+        usersViewModel.prepareUsers(intent)
+        albumsViewModel.prepareAlbums(intent)
+        photosViewModel.preparePhotos(intent)
     }
-
 
 }
--- app/src/main/java/dog/snow/androidrecruittest/ui/ListFragment.kt
+++ app/src/main/java/dog/snow/androidrecruittest/ui/ListFragment.kt
@@ -1,10 +1,89 @@
 package dog.snow.androidrecruittest.ui
 
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
 import dog.snow.androidrecruittest.R
+import dog.snow.androidrecruittest.repository.model.RawAlbum
+import dog.snow.androidrecruittest.repository.model.RawPhoto
+import dog.snow.androidrecruittest.ui.adapter.ListAdapter
+import dog.snow.androidrecruittest.ui.model.ListItem
+import dog.snow.androidrecruittest.viewmodels.AlbumsViewModel
+import dog.snow.androidrecruittest.viewmodels.ListItemsViewModel
+import dog.snow.androidrecruittest.viewmodels.PhotosViewModel
 import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ListFragment : Fragment(R.layout.list_fragment) {
 
+    private val listItemsViewModel: ListItemsViewModel by viewModel()
+    private val albumsViewModel: AlbumsViewModel by sharedViewModel()
+    private val photosViewModel: PhotosViewModel by sharedViewModel()
 
+    private val adapter = ListAdapter {i, p, v -> onClickItemList(i, p, v)}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val viewGroup = this.view
+        if(viewGroup is ViewGroup) {
+            val recyclerView = viewGroup.findViewById<RecyclerView>(R.id.rv_items)
+            recyclerView.adapter = adapter
+        }
+
+        listItemsViewModel.listItems.observe(this.viewLifecycleOwner, Observer { listItems -> updateItemListAdapter(listItems) })
+
+        albumsViewModel.albums.observe(this.viewLifecycleOwner, Observer { albums ->
+            photosViewModel.photos.value?.let {
+                updateItemsListViewModel(albums, it)
+            }
+        })
+
+        photosViewModel.photos.observe(this.viewLifecycleOwner, Observer { photos ->
+            albumsViewModel.albums.value?.let {
+                updateItemsListViewModel(it, photos)
+            }
+        })
+
+        tryUpdatingBefore()
+    }
+
+    /**
+     * To try to update the list based on view models without
+     * waiting for observers to fire, in case the data is ready
+     * already.
+     */
+    private fun tryUpdatingBefore() {
+        listItemsViewModel.listItems.value?.let {
+            updateItemListAdapter(it)
+            return
+        }
+        albumsViewModel.albums.value?.let {albums ->
+            photosViewModel.photos.value?.let {photos ->
+                updateItemsListViewModel(albums, photos)
+            }
+        }
+    }
+
+    private fun updateItemsListViewModel(albums: Map<Int, RawAlbum>, photos: List<RawPhoto>) {
+        listItemsViewModel.prepareListItems(photos, albums)
+    }
+
+    private fun updateItemListAdapter(list: List<ListItem>) {
+        val viewGroup = this.view
+        if(viewGroup is ViewGroup) {
+            val recyclerView = viewGroup.findViewById<RecyclerView>(R.id.rv_items)
+            (recyclerView.adapter as ListAdapter).submitList(list)
+            (recyclerView.adapter as ListAdapter).notifyDataSetChanged()
+        }
+    }
+
+    private fun onClickItemList(i: ListItem, pos: Int, v: View) {
+        Toast.makeText(this.context, "Clicked ${i.title}", Toast.LENGTH_SHORT).show()
+    }
 }
