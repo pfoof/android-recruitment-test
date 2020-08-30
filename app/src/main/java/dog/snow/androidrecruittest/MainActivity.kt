a9f58598f9ce6d555ac723ed8ab37621d5ca38e4
pfoof <piotr@pabis.eu>
2020-08-30 14:07:20 +0200

    Optimize imports

--- app/src/main/java/dog/snow/androidrecruittest/MainActivity.kt
+++ app/src/main/java/dog/snow/androidrecruittest/MainActivity.kt
@@ -2,17 +2,10 @@ package dog.snow.androidrecruittest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import dog.snow.androidrecruittest.repository.CollectionToTreeMap
-import dog.snow.androidrecruittest.repository.model.RawAlbum
-import dog.snow.androidrecruittest.repository.model.RawPhoto
-import dog.snow.androidrecruittest.repository.model.RawUser
-import dog.snow.androidrecruittest.ui.model.ListItem
 import dog.snow.androidrecruittest.viewmodels.AlbumsViewModel
-import dog.snow.androidrecruittest.viewmodels.ListItemsViewModel
 import dog.snow.androidrecruittest.viewmodels.PhotosViewModel
 import dog.snow.androidrecruittest.viewmodels.UsersViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.util.*
 
 class MainActivity : AppCompatActivity(R.layout.main_activity){
 
@@ -28,6 +21,7 @@ class MainActivity : AppCompatActivity(R.layout.main_activity){
         usersViewModel.prepareUsers(intent)
         albumsViewModel.prepareAlbums(intent)
         photosViewModel.preparePhotos(intent)
+
     }
 
 }
